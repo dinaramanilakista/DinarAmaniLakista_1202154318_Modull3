547a9b020e0bc8b5d37353472258d22cbd5ba9b1
Dinar Amani Lakista <lakistadinaramani@gmail.com>
2018-02-25 15:15:44 +0700

    push pertama kali

---
+++ app/src/main/java/com/a45u/android/dinaramanilakista_1202154318_modul3/SplashScreen.java
@@ -0,0 +1,24 @@
+package com.a45u.android.dinaramanilakista_1202154318_modul3;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        //handle untuk memulai MainActivity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                startActivity(new Intent(SplashScreen.this, LoginActivity.class));
+                finish();
+            }
+        }, 1000);
+    }
+}
